b27b1069884ba92c012038259a649dbf30d7dfa8
SiddharthaShankarSwain <2000030933@kluniversity.in>
2023-04-16 22:39:58 +0530

    siddhu here

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,9 @@ const App = () => {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 relative">
-        <Sidebar />
+        <Sidebar/>
       </div>
+      {/* siddhu */}
 
       <div className="flex-1 max-sm:w-full max-2-[1280px] mx-auto sm:pr-5">
         <Navbar />
