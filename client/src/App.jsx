3956c9ab52bb06541d2140c352dcc8f313652da1
Nanda Kishore <namburinandakishore.com>
2023-04-16 22:55:22 +0530

    hello nandu here

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,13 @@ const App = () => {
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 relative">
         <Sidebar/>
-      </div>
+      </div >
       {/* siddhu */}
 
       <div className="flex-1 max-sm:w-full max-2-[1280px] mx-auto sm:pr-5">
         <Navbar />
+        
+        {/* nandu */}
 
         <Routes>
           <Route path="/" element={<Home />} />
